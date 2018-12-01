a66383c4fdaa1ff0890acb0ad1375fd8b684796a
Dulet <duleet@gmail.com>
2018-12-01 23:48:49 +0100

    it doesnt work lmao

--- core.py
+++ core.py
@@ -7,15 +7,42 @@ for line in array:
 for a in range(0, len(dab)):
     dab[a] = dab[a][:-1]
 
+resultarray = [0]
 result = 0
-for a in range(0, len(dab)):
-    if "+" in dab[a]:
-        dab[a] = dab[a][1:]
-        dab[a] = int(float(dab[a]))
-        result += dab[a]
+s = set(resultarray)
+value = 0
+yay = 0
+def loop(result, value = value, yay = yay):
+    for a in range(0, len(dab)):
+        if "+" in dab[a]:
+            la = dab[a][1:]
+            ma = int(float(la))
+            result += ma
+            if result in resultarray:
+                print("AAAAAAAAA")
+                value += result
+                yay += 1
+                return "DINGDINGDING: " + str(result)
+            if result not in s:
+                resultarray.append(result)
 
-    elif "-" in dab[a]:
-        dab[a] = dab[a][1:]
-        dab[a] = int(float(dab[a]))
-        result -= dab[a]
+        elif "-" in dab[a]:
+            al = dab[a][1:]
+            wa = int(float(al))
+            print(wa)
+            print(result)
+            result -= wa
+            if result in resultarray:
+                print("AAAAAAA")
+                value += result
+                yay += 1
+                return "DINGDINGDING: " + str(result)
+            if result not in s:
+                resultarray.append(result)
+    return result
 
+for f in range(0, 100000):
+    result = loop(result)
+    print(value)
+    if yay is not 0:
+        break
